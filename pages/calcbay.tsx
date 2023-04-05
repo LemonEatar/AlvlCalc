a55a3313fdcdce5c258bb97ad0b0be720fd8a54c
LemonEatar <jakstr@outlook.de>
2023-04-05 16:04:42 +0200

    bug fixes #1

--- pages/calcbay.tsx
+++ pages/calcbay.tsx
@@ -2,24 +2,22 @@ export default function BayCalc() {
   return (
     <div>
       <title>aLvlCalc Bayern</title>
-      <body>
-        <table>
-          <thead>
-            <tr>
-              <th colSpan={1}>Mathe</th>
-              <th>Deutsch</th>
-              <th>Chemier</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>Test</td>
-              <td>Test2</td>
-              <td>Test3</td>
-            </tr>
-          </tbody>
-        </table>
-      </body>
+      <table>
+        <thead>
+          <tr>
+            <th colSpan={1}>Mathe</th>
+            <th>Deutsch</th>
+            <th>Chemier</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Test</td>
+            <td>Test2</td>
+            <td>Test3</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 }
