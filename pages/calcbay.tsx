c13ff9934f60aa45ed18ce453046b32e87ccde17
LemonEatar <jakstr@outlook.de>
2023-04-05 15:49:38 +0200

    i hate my life (added the table and now it should work)

--- pages/calcbay.tsx
+++ pages/calcbay.tsx
@@ -1,3 +1,25 @@
-export default function bayCalc() {
-  return <div>test</div>;
+export default function BayCalc() {
+  return (
+    <div>
+      <title>aLvlCalc Bayern</title>
+      <body>
+        <table>
+          <thead>
+            <tr>
+              <th colSpan={1}>Mathe</th>
+              <th>Deutsch</th>
+              <th>Chemier</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Test</td>
+              <td>Test2</td>
+              <td>Test3</td>
+            </tr>
+          </tbody>
+        </table>
+      </body>
+    </div>
+  );
 }
