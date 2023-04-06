84e6fb0def0cc18dd0258d6243b8eb482c090d29
LemonEatar <jakstr@outlook.de>
2023-04-06 10:23:18 +0200

    it works ^^

--- pages/calcbay.tsx
+++ pages/calcbay.tsx
@@ -3,9 +3,14 @@ export default function BayCalc() {
     <div>
       <title>aLvlCalc Bayern</title>
       <table>
-        <tr>
-          <th>Mathe</th> <td>2</td>
-        </tr>
+        <tbody>
+          <tr>
+            <th>maths: </th> <td>2</td>
+          </tr>
+          <tr>
+            <th>chemics: </th> <td>15</td>
+          </tr>
+        </tbody>
       </table>
     </div>
   );
