8607e999742602595d0f13e8b68f680aef24ae1a
LemonEatar <jakstr@outlook.de>
2023-04-06 11:34:39 +0200

    a good old input field that takes value and adds them to a table

--- pages/calcbay.tsx
+++ pages/calcbay.tsx
@@ -1,17 +1,47 @@
-export default function BayCalc() {
+import { useState } from "react";
+
+interface TableData {
+  id: number;
+  value: string;
+}
+
+const Table = () => {
+  const [tableData, setTableData] = useState<TableData[]>([]);
+  const [inputValue, setInputValue] = useState("");
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleAddRow = () => {
+    const newTableData = [...tableData];
+    newTableData.push({ id: Date.now(), value: inputValue });
+    setTableData(newTableData);
+    setInputValue("");
+  };
+
   return (
     <div>
-      <title>aLvlCalc Bayern</title>
+      <input value={inputValue} onChange={handleInputChange} />
+      <button onClick={handleAddRow}>Add</button>
       <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Value</th>
+          </tr>
+        </thead>
         <tbody>
-          <tr>
-            <th>maths: </th> <td>2</td>
-          </tr>
-          <tr>
-            <th>chemics: </th> <td>15</td>
-          </tr>
+          {tableData.map((row) => (
+            <tr key={row.id}>
+              <td>{row.id}</td>
+              <td>{row.value}</td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
   );
-}
+};
+
+export default Table;
