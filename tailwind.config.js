366a64ba56b8e012a614a0d1d91bcdc98e8b6ca1
DoctorManhattan123 <alexandervonberkov1@gmail.com>
2023-04-07 11:04:18 +0200

    feat: get daisyui to work]

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,12 +12,4 @@ module.exports = {
     extend: {},
   },
   plugins: [require("daisyui")],
-  daisyui: {
-    styled: true,
-    themes: true,
-    base: true,
-    utils: true,
-    logs: true,
-    rtl: false,
-  },
 };
